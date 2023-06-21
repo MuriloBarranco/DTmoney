ccdf3669eef630d24b90426254f66ea5570be847
Murilo Barranco <murilobarranco@MacBook-Pro-de-Murilo.local>
2023-06-20 22:14:26 -0300

    feature new classroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { Dashboard } from "./components/Dashboard";
-import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
+import { useState } from "react"
+
+import { Header } from "./components/Header"
+import { Dashboard } from "./components/Dashboard"
+
+import Modal from "react-modal"
+
+import { GlobalStyle } from "./styles/global"
+import { NewTransactionModal } from "./components/NewTransactionModal"
+
+Modal.setAppElement("#root")
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
-      <Dashboard/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
-  );
+  )
 }
